b7991e9d245eda82476d0f0e809029993a21bb6b
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-04 13:41:46 +0430

    add customer models

--- customer/models.py
+++ customer/models.py
@@ -4,7 +4,8 @@ from core.models import User
 # Create your models here.
 class customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
+    age = models.IntegerField(null=True, blank=True)
+    weight = models.IntegerField(null=True, blank=True)
+    height = models.IntegerField(null=True, blank=True)
     def __str__(self):
         return f'{self.user.username}'
-
