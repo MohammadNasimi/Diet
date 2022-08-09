97b2a576ed4fb5e905dbc75e3221c9dc1b528103
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-09 16:51:06 +0430

    add calculate in views Diet

--- Diet/views.py
+++ Diet/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,response
 from Diet.models import Diet
+from Diet.calculate_diet import calculate
 # Create your views here.
 from Diet.serializers import DietSerializers
-class DietApi(generics.CreateAPIView):  
+class DietApi(generics.ListCreateAPIView):  
 
     serializer_class = DietSerializers
     queryset = Diet.objects.all()
 
     def post(self, request, *args, **kwargs):
+    
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        
+
         Diet_data = request.data
-        print(Diet_data)
-        return self.create(request, *args, **kwargs)
+        calculate(Diet_data)
+        return response.Response(serializer.data,status = 200)
