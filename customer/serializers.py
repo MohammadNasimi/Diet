666fb5da1db37d8ae9a635d2049b805bcc000d63
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-09 14:08:31 +0430

    ser User

--- customer/serializers.py
+++ customer/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
 from customer.models import customer
-
+from core.serializers import UserSerializers
 class customerSerializers(serializers.ModelSerializer):
+    user = UserSerializers()
     class Meta:
         model= customer
-        fields = '__all__'
-    
+        fields = ['user','age','weight','height']
