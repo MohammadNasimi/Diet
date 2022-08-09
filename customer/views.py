f6cb5022b08a8714a9688ed22fb5e46b1ed6680e
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-09 16:04:58 +0430

    post create customer

--- customer/views.py
+++ customer/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from customer.serializers import customerSerializers
 from customer .models import customer
+from core.models import User
 # Create your views here.
 from rest_framework import generics , response
-
+from django.contrib.auth import get_user_model
 class customerAPI(generics.ListCreateAPIView):
     serializer_class = customerSerializers
     queryset = customer.objects.all()
@@ -12,8 +12,25 @@ class customerAPI(generics.ListCreateAPIView):
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+
+        # serializer = self.get_serializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)
+        # serializer.save()
+        # headers = self.get_success_headers(serializer.data)
         serializer = self.get_serializer(data=request.data)
+
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-        headers = self.get_success_headers(serializer.data)
-        return response.Response(serializer.data, status=200, headers=headers)
+        customer_data = request.data
+        user = User.objects.create_user(username=customer_data['user']['username'],
+         password=customer_data['user']['password'],kind_user =customer_data['user']['kind_user'])
+        user.save()
+        customer_create  = customer.objects.create(user = user,age = customer_data['age']
+        ,weight=customer_data['weight'],height=customer_data['height'])
+        customer_create.save()
+        # print(customer_data['user']['username'])
+        # print(customer_data['age'])
+        # print(customer_data['weight'])
+        # print(customer_data['height'])
+
+
+        return response.Response(serializer.data,status = 200)
