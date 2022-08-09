2edaf93f916301fb8b1913f6dcc48856858b2124
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-09 16:25:08 +0430

    view post Diet

--- Diet/views.py
+++ Diet/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from Diet.models import Diet
 # Create your views here.
+from Diet.serializers import DietSerializers
+class DietApi(generics.CreateAPIView):  
+
+    serializer_class = DietSerializers
+    queryset = Diet.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        
+        Diet_data = request.data
+        print(Diet_data)
+        return self.create(request, *args, **kwargs)
