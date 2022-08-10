9d932250d176b0bad18a6687a23929875dc2b95a
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-10 13:28:57 +0430

    food_diet_user admin

--- food/admin.py
+++ food/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from food.models import food
+from food.models import food ,food_diet_user
 
 # Register your models here.
 class foodAdmin(admin.ModelAdmin):
@@ -8,3 +8,9 @@ class foodAdmin(admin.ModelAdmin):
     list_filter = ['name']
     search_fields = ['name']
 admin.site.register(food,foodAdmin)
+
+class food_diet_userAdmin(admin.ModelAdmin):
+    fields = ['customer_food','food_field','mesure_food_customer']
+    list_filter = ['customer_food']
+    search_fields = ['customer_food']
+admin.site.register(food_diet_user,food_diet_userAdmin)
