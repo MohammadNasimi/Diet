6d98c48bfd1f18088f6d024775139e4a696493a9
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-10 13:28:46 +0430

    food_diet_user models

--- food/models.py
+++ food/models.py
@@ -1,5 +1,7 @@
+from operator import mod
+from statistics import mode
 from django.db import models
-
+from customer.models import customer
 # Create your models here.
 class food (models.Model):
     name = models.CharField(max_length=20,null=False)
@@ -9,3 +11,10 @@ class food (models.Model):
     def __str__(self) -> str:
         return f'{self.name},{self.calories}'
         
+class food_diet_user(models.Model):
+    customer_food = models.ForeignKey(customer, on_delete=models.RESTRICT)
+    food_field = models.ForeignKey(food,on_delete=models.RESTRICT)
+    mesure_food_customer = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.food_field},{self.mesure_food_customer}'
