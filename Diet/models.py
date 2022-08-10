d0adbce138a8240c2e98397839981e60707f4444
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-10 13:45:30 +0430

    Diet change food_exercise fields

--- Diet/models.py
+++ Diet/models.py
@@ -1,13 +1,13 @@
 
 from django.db import models
 from core.models import User
-from food.models import food
-from exercise.models import exercise
+from food.models import food_diet_user
+from exercise.models import exercise_diet_user
 # Create your models here.
 class Diet(models.Model):
     user =models.ForeignKey(User, on_delete=models.RESTRICT,null=True,blank=True)
-    food_Diet = models.ManyToManyField(food)
-    exercise_Diet = models.ManyToManyField(exercise)
+    food_Diet = models.ManyToManyField(food_diet_user)
+    exercise_Diet = models.ManyToManyField(exercise_diet_user)
 
     def __str__(self):
         return f'{self.user}'   
