f72288f7b669cb97b6307588a3973d29de71515a
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-10 11:12:57 +0430

    add BMi customer in admin panel

--- customer/admin.py
+++ customer/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from customer.models import customer
 # Register your models here.
 class CustomerAdmin(admin.ModelAdmin):
-    fields = ['user','age','weight','height']
-    list_filter = ['user']
+    fields = ['user','age','weight','height','BMI']
+    list_filter = ['user','BMI']
     search_fields = ['user']
 
 
