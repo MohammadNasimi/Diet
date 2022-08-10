a061cffb64d5b74e0cdeadab0e6b2ab790293e87
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-10 13:01:34 +0430

    exercise_diet_user models

--- exercise/models.py
+++ exercise/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from customer.models import customer
 # Create your models here.
 class exercise (models.Model):
     name_exercise = models.CharField(max_length=20,null=False)
@@ -8,4 +9,8 @@ class exercise (models.Model):
 
     def __str__(self) -> str:
         return f'{self.name_exercise}'
-        
+
+class exercise_diet_user(models.Model):
+    customer_exercise = models.ForeignKey(customer, on_delete=models.RESTRICT)
+    exersise_field = models.ForeignKey(exercise,on_delete=models.RESTRICT)
+    time_exercise_customer = models.IntegerField()
