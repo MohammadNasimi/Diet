b54519e4daa6ba28e62503b49415d696765c2ee1
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-10 13:01:43 +0430

    exercise_diet_user ser

--- exercise/serializers.py
+++ exercise/serializers.py
@@ -1,8 +1,14 @@
+from dataclasses import field
 from rest_framework import serializers
 
-from exercise.models import exercise
+from exercise.models import exercise ,exercise_diet_user
 
 class exerciseSerializers(serializers.ModelSerializer):
     class Meta:
         model= exercise
         fields = ['name_exercise','calories','time']
+
+class exercise_dietSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = exercise_diet_user
+        fields = '__all__'
