7b7c483d88854932359da1488c168de1d7dc513e
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-10 13:01:04 +0430

    exercise_diet_user admin

--- exercise/admin.py
+++ exercise/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from exercise.models import exercise
+from exercise.models import exercise,exercise_diet_user
 # Register your models here.
 class exerciseAdmin(admin.ModelAdmin):
     fields = ['name_exercise','calories','time']
@@ -11,3 +11,11 @@ class exerciseAdmin(admin.ModelAdmin):
 
 
 admin.site.register(exercise,exerciseAdmin)
+
+class exercise_diet_userAdmin(admin.ModelAdmin):
+    fields = ['customer_exercise','exersise_field','time_exercise_customer']
+    list_filter = ['customer_exercise']
+    search_fields = ['customer_exercise']
+
+
+admin.site.register(exercise_diet_user,exercise_diet_userAdmin)
