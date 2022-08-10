5c10dd4d69dc514898cb3913babd3dbe7c01a483
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-10 15:52:03 +0430

    DietSerializers

--- Diet/serializers.py
+++ Diet/serializers.py
@@ -1,7 +1,19 @@
+import imp
 from rest_framework import serializers
-
+from food.serializers import food_diet_user_Serializers
+from exercise.serializers import exercise_dietSerializers
 from Diet.models import Diet
+from food.models import food
 class DietSerializers(serializers.ModelSerializer):
+    food_Diet = food_diet_user_Serializers(many=True)
+    exercise_Diet=exercise_dietSerializers(many =True)
     class Meta:
         model= Diet
-        fields = "__all__"
+        fields = ['user','food_Diet','exercise_Diet']
+        
+    # def get_accounts_items(self, obj):
+    #     customer_account_query = food.objects.filter(obj.id)
+    #     serializer = food_diet_user_Serializers(customer_account_query, many=True)
+    #     return serializer.data
+
+
