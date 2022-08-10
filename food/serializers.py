6708de973cc3c2885b9f3024321427c7ac8494fa
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-10 13:29:07 +0430

    food_diet_user ser

--- food/serializers.py
+++ food/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 
-from food.models import food
+from food.models import food,food_diet_user
 
 class foodSerializers(serializers.ModelSerializer):
     class Meta:
         model= food
         fields = ['name','calories','mesure']
+
+class food_diet_user_Serializers(serializers.ModelSerializer):
+    class Meta:
+        model = food_diet_user
+        fields = "__all__"
+        
