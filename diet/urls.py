8abd198a6853a03b3da57a413841ba3ef27df7c9
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-11 12:18:18 +0430

    Diet_admin urls

--- diet/urls.py
+++ diet/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from exercise.views import exercise_api, exercise_diet_admin_api,exercise_diet_api,exercise_diet_adminSerializers
 from food.views import foodAPI ,food_diet_userApi,food_diet_adminApi
 from customer.views import customerAPI
-from Diet.views import DietApi
+from Diet.views import DietApi,Diet_adminApi
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('exercise_api/',exercise_api,name= 'exercise_api'),
@@ -29,5 +29,6 @@ urlpatterns = [
     path('exercise_diet_user',exercise_diet_api.as_view(),name = 'exercise_diet_user'),
     path('food_diet_user',food_diet_userApi.as_view(),name = 'food_diet_user'),
     path('food_diet_admin',food_diet_adminApi.as_view(),name ='food_diet_admin'),
-    path('exercise_diet_admin',exercise_diet_admin_api.as_view(),name = 'exerciser_diet_admin')
+    path('exercise_diet_admin',exercise_diet_admin_api.as_view(),name = 'exerciser_diet_admin'),
+    path('Diet_adminApi',Diet_adminApi.as_view(),name='Diet_adminApi')
 ]
