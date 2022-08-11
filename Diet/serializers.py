ae3e9ea2be48143a21cdf6abc805bd8f79a82aa8
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-11 12:17:59 +0430

    Diet_admin ser

--- Diet/serializers.py
+++ Diet/serializers.py
@@ -1,8 +1,8 @@
 import imp
 from rest_framework import serializers
-from food.serializers import food_diet_user_Serializers
-from exercise.serializers import exercise_dietSerializers
-from Diet.models import Diet
+from food.serializers import food_diet_user_Serializers,food_diet_admin_Serializers
+from exercise.serializers import exercise_dietSerializers,exercise_diet_adminSerializers
+from Diet.models import Diet_admin,Diet
 from food.models import food
 class DietSerializers(serializers.ModelSerializer):
     food_Diet = food_diet_user_Serializers(many=True)
@@ -15,5 +15,10 @@ class DietSerializers(serializers.ModelSerializer):
     #     customer_account_query = food.objects.filter(obj.id)
     #     serializer = food_diet_user_Serializers(customer_account_query, many=True)
     #     return serializer.data
-
+class Diet_adminSerializers(serializers.ModelSerializer):
+    food_Diet_admin = food_diet_admin_Serializers(many=True)
+    exercise_Diet_admin=exercise_diet_adminSerializers(many =True)
+    class Meta:
+        model= Diet_admin
+        fields = ['user_admin','food_Diet_admin','exercise_Diet_admin']
 
