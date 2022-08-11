9433cfeceea5537a63dd22f2e24052d9cb09eb1f
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-11 11:28:27 +0430

    food_diet_admin ser

--- food/serializers.py
+++ food/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from food.models import food,food_diet_user
+from food.models import food,food_diet_user,food_diet_admin
 
 class foodSerializers(serializers.ModelSerializer):
     class Meta:
@@ -11,4 +11,8 @@ class food_diet_user_Serializers(serializers.ModelSerializer):
     class Meta:
         model = food_diet_user
         fields = "__all__"
-        
+
+class food_diet_admin_Serializers(serializers.ModelSerializer):
+    class Meta:
+        model = food_diet_admin
+        fields = "__all__"
