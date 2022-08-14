7ea9f0fd5ab5586b2745d1d5511115e2290ae5f8
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-14 17:10:38 +0430

    change model Diet admin

--- Diet/models.py
+++ Diet/models.py
@@ -15,7 +15,7 @@ class Diet(models.Model):
 class Diet_admin(models.Model):
     user_admin =models.ForeignKey(customer, on_delete=models.RESTRICT,null=True,blank=True)
     food_Diet_admin = models.ManyToManyField(food_diet_admin)
-    exercise_Diet_admin = models.ManyToManyField(exercise_diet_user)
+    exercise_Diet_admin = models.ManyToManyField(exercise_diet_admin)
 
     def __str__(self):
-        return f'{self.user}' 
+        return f'{self.user_admin}' 
