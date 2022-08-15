4f2525c4f297347e3ca105700bdedf4ec78c0a70
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-15 12:43:25 +0430

    url pk Diet_adminApi

--- diet/urls.py
+++ diet/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('food_diet_user',food_diet_userApi.as_view(),name = 'food_diet_user'),
     path('food_diet_admin',food_diet_adminApi.as_view(),name ='food_diet_admin'),
     path('exercise_diet_admin',exercise_diet_admin_api.as_view(),name = 'exerciser_diet_admin'),
-    path('Diet_adminApi',Diet_adminApi.as_view(),name='Diet_adminApi')
+    path('Diet_adminApi/<int:customer_id>/',Diet_adminApi.as_view(),name='Diet_adminApi')
 ]
