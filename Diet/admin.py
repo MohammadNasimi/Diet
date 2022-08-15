3ca0d96b28de7520e2b135085a66495c9620cf12
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-15 13:34:04 +0430

    kind_diet add in admin

--- Diet/admin.py
+++ Diet/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from Diet.models import Diet,Diet_admin
 # Register your models here.
 class DietAdmin(admin.ModelAdmin):
-    fields = ['user','food_Diet','exercise_Diet']
+    fields = ['user','food_Diet','exercise_Diet','kind_diet']
     list_filter = ['user']
     search_fields = ['user']
 admin.site.register(Diet,DietAdmin)
