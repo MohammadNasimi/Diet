1be8bed025b954b95ffdc82250ae6ddff59dc910
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-17 15:10:30 +0430

    loginserializers ser

--- customer/serializers.py
+++ customer/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 
 from customer.models import customer
 from core.serializers import UserSerializers
+from core.models import User
 class customerSerializers(serializers.ModelSerializer):
     user = UserSerializers()
     class Meta:
         model= customer
         fields = ['user','age','weight','height','BMI']
+
+class loginserializers(serializers.ModelSerializer):
+    class Meta:
+        model= User
+        fields = ['username','password','token']
+        read_only_fields =['token']
