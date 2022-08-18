dc31b4e68fba4b64839f658df14372ba156dc6f5
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-18 12:07:33 +0430

    permissions Diet

--- Diet/permissions.py
+++ Diet/permissions.py
@@ -4,11 +4,29 @@ class custompermissions(BasePermission):
         """
         Return `True` if permission is granted, `False` otherwise.
         """
-        if request.method in SAFE_METHODS :
+        if request.method in SAFE_METHODS and request.user:
             print(request.user)
             return True
-        else:
-            pass
+        elif request.method  == 'POST' and request.user:
+            print(request.user.kind_user)
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return True
+
+class custompermissionsDiet_admin(BasePermission):
+    def has_permission(self, request, view):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        if request.method in SAFE_METHODS and request.user:
+            print(request.user)
+            return True
+        elif request.method  == 'POST' and request.user.kind_user == "1":
+            return True
 
     def has_object_permission(self, request, view, obj):
         """
