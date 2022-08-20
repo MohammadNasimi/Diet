38b27024f274a972a8c3a91f4b213fff4dc101e7
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-20 15:21:12 +0430

    change sqlite  to postgress

--- diet/settings.py
+++ diet/settings.py
@@ -81,13 +81,22 @@ WSGI_APPLICATION = 'diet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'akurzeqq',
+        'USER': 'akurzeqq',
+        'PASSWORD': 'bUcqfJyPN0ftIjF6f69ClJ3u56CDPXOr',
+        'HOST': 'suleiman.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
