7802a4d5c8ef044d5b858d253e102708ce61f5e7
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-08-20 12:26:34 +0430

    add permisssion for Diet

--- Diet/permissions.py
+++ Diet/permissions.py
@@ -1,14 +1,18 @@
 from rest_framework.permissions import BasePermission ,SAFE_METHODS
+
+
 class custompermissions(BasePermission):
     def has_permission(self, request, view):
         """
         Return `True` if permission is granted, `False` otherwise.
         """
+        #############################################
+
+        ##################################
         if request.method in SAFE_METHODS and request.user:
-            print(request.user)
             return True
+        
         elif request.method  == 'POST' and request.user:
-            print(request.user.kind_user)
             return True
 
     def has_object_permission(self, request, view, obj):
@@ -22,8 +26,10 @@ class custompermissionsDiet_admin(BasePermission):
         """
         Return `True` if permission is granted, `False` otherwise.
         """
+        #################################
+
+        ###################################
         if request.method in SAFE_METHODS and request.user:
-            print(request.user)
             return True
         elif request.method  == 'POST' and request.user.kind_user == "1":
             return True
